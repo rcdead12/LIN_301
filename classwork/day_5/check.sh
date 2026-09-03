2edef37babdfe0590d46dc61e9e726f75b4a8f77
Rupert Clark <321115178+rcdead12@users.noreply.github.com>
2026-09-03 11:20:00 -0400

    Day 5 in class work completed

---
+++ classwork/day_5/check.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Listing files within directory day5_files"
+ls -l day5_files
+tree day5_files
---
+++ classwork/day_5/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                 # tells the system to run this file with bash
+echo "This is my first script."                     # print a message
+curl -o term_comm.txt https://winjapati.github.io/courses/301/term_comm.txt  # download the file
+mkdir folder_1                                      # make a new folder
+echo term_comm.txt                                  # print the filename
+mv term_comm.txt folder_1                           # move it into folder_1
