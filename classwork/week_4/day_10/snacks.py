5cc1c9b6b83024eebbc31069b905bc7153a715ba
Rupert Clark <321115178+rcdead12@users.noreply.github.com>
2026-09-16 13:41:33 -0400

    day 10 classwork completed

---
+++ classwork/week_4/day_10/snacks.py
@@ -0,0 +1,7 @@
+SNACK_BUDGET = 20       #uppercase = fixed variable
+
+popcorn_budget = 8
+candy_budget = 3
+
+money_left = SNACK_BUDGET - popcorn_budget - candy_budget
+print(money_left)
--- classwork/week_4/day_9/regex_practice.py
+++ classwork/week_4/day_9/regex_practice.py
@@ -2,3 +2,7 @@ import re                                               #loads python's regex to
 
 with open("../../../data/gutenburg/alice.txt", encoding="utf-8") as f:  #opens alice.txt
     text = f.read()                                     #reads the whole file into one string, called 'text'
+
+matches_a_q = re.findall(r"[a-q]at", text)   # finds "cat", "cot", or "cut"
+print(len(matches_a_q))                      # counts how many were found
+print(*matches_a_q, sep="\n")                # prints each match on a separate line
